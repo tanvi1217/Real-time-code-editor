b17c1c7b50865c81e3f46badac9cadcdca32e6c0
tanvi1217 <tanvisolanke17@gmail.com>
2026-02-15 14:26:26 +0530

    Add files via upload

---
+++ coe.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter n :";
+    cin>>n;
+    if(n%5==0){
+        cout<<"divisible by 5";
+    }
+    else{
+        cout<<"not divisible by 5";
+    } 
+    
+}
